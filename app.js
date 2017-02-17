844df531364f48bd211c74cf170465067ea83f99
Joshric Aurea <joshricaurea@yahoo.com>
2017-02-17 01:11:08 -0800

    fix app.js to listen to heroku port

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get('/portfolio', function( req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
+app.listen( process.env.PORT || 3000, function () {
   console.log('App listening on port 3000!');
 });
